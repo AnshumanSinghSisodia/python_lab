1b0688e9bfcfb1a67d398e860ccd77b77e49237a
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2025-03-26 23:21:26 +0530

    creating package

---
+++ setup.py
@@ -0,0 +1,37 @@
+'''
+The setup.py file is an essential part of packaging and distributing Python projects. It is used by setuptools 
+(or disutils in older Python versions) to define the configuration of your project, 
+such as its metadata, dependencies, and more
+'''
+
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements()-> List[str]:
+    """
+    This will return list of requirements.
+    """
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            #read lines from the file
+            lines = file.readlines()
+            ## Process each line
+            for line in lines:
+                requirement=line.strip()
+                ## ignore empty lines and -e .
+                if requirement and requirement!='-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+    
+    return requirement_lst
+
+setup(
+    name="SampleProject",
+    version="0.0.1",
+    author="Anshuman Singh Sisodia",
+    author_email="singhsisodiaanshuman@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
